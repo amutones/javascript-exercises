d50b884700905a4a40ae931e5c81fac8d01d0f98
amutones <aurgena@gmail.com>
2024-09-13 17:26:46 -0500

    Created function to convert to Celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * (5/9);
+  return Math.round(conversion*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
 };
 
 // Do not edit below this line
