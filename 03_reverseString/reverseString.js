fd01549ac01526283423ad52d50f9ed129560bc8
amutones <aurgena@gmail.com>
2024-09-13 14:27:07 -0500

    Completed coding with passed testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textArr = [];
+    for (let i = 0; i < text.length; i++) {
+       textArr.unshift(text.charAt(i));
+    }
+    return textArr.join("");
 };
 
 // Do not edit below this line
