0c9ff2034abbf0010a064907fba1d197d1615d7e
amutones <aurgena@gmail.com>
2024-09-13 15:02:05 -0500

    Created function to sum all between 1 and a number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let largerNumber = num1;
+    if (num1 <0 || num2 <0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    if (num2 > num1) {
+        largerNumber = num2;
+    }
+    return largerNumber*(largerNumber + 1)/2;
 };
 
 // Do not edit below this line
