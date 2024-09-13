c7f1de35d462ac7be1cb18345716db6f4141aabe
amutones <aurgena@gmail.com>
2024-09-13 14:52:17 -0500

    Passed all tests for removing from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...item) {
+    for (let i = 0; i < item.length; i++) {
+        while(arr.indexOf(item[i]) !=-1) {
+            let position = 0;
+            position = arr.indexOf(item[i]);
+            arr.splice(position, 1);
+        }
+    } 
+    return arr;
 };
 
 // Do not edit below this line
